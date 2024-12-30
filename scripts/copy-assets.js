f37a77b379112227f4937854442e98cad4d634a0
2sharr <tusharshinde186306@gmail.com>
2024-12-30 13:37:13 +0530

    Fix: Update copy-assets script to use ES modules

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -1,5 +1,9 @@
-const fs = require('fs-extra');
-const path = require('path');
+import fs from 'fs-extra';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Copy public folder to dist
 fs.copySync('public', 'dist', {
